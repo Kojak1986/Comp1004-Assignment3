1a4663e310169ae097d2f52cdd6b053d041e095a
Kojak420 <klkovacs86@gmail.com>
2016-11-07 12:45:09 -0500

    Still needs work on the add dvd logic in calculate method

--- Comp1004-Assignment3/OrderForm.cs
+++ Comp1004-Assignment3/OrderForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Comp1004_Assignment3
 {
-   
+
     public partial class OrderForm : Form
     {
         SelectionForm select = new SelectionForm();
@@ -32,49 +32,59 @@ namespace Comp1004_Assignment3
 
         private void OrderForm_Load(object sender, EventArgs e)
         {
-            
+
 
             TitleTextBox.Text = Program.info.Title;
             GenreTextBox.Text = Program.info.Genre;
             CostTextBox.Text = Program.info.Cost;
             OrderPictureBox.Image = Program.info.Picture;
-           
+
             CalculateTotals();
         }
 
         private void CalculateTotals()
         {
+            
             double TotalTax = Convert.ToDouble((CostTextBox.Text as string).TrimStart('$'));
             double Grand = TotalTax * 1.13;
 
-            TotalTax = TotalTax * 0.13;
+            if (OrderDvdCheckBox.Checked == true)
+            {
+                double DvdCost = TotalTax + 10;
 
-            SubtotalTextBox.Text = CostTextBox.Text;
-            SalesTaxTextBox.Text = TotalTax.ToString("C2");
-            GrandTotalTextBox.Text = Grand.ToString("C2");
+                CostTextBox.Text = DvdCost.ToString("C2");
+                SubtotalTextBox.Text = CostTextBox.Text;
+                SalesTaxTextBox.Text = TotalTax.ToString("C2");
+                GrandTotalTextBox.Text = Grand.ToString("C2");
+
+            }
+            else if(OrderDvdCheckBox.Checked == false)
+            {
+
+                TotalTax = TotalTax * 0.13;
+
+                SubtotalTextBox.Text = CostTextBox.Text;
+                SalesTaxTextBox.Text = TotalTax.ToString("C2");
+                GrandTotalTextBox.Text = Grand.ToString("C2");
+            }
 
         }
 
         //Back button returns you to the previous form.
         private void BackButton_Click(object sender, EventArgs e)
         {
-            
+
 
             select.Show();
             this.Hide();
 
             select.ReturnInfo();
 
-           
+
         }
 
         private void OrderDvdCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            double dvd = Convert.ToDouble((SubtotalTextBox.Text as string).TrimStart('$'));
-
-            dvd = dvd + 10;
-
-            SubtotalTextBox.Text = dvd.ToString("C2");
             CalculateTotals();
         }
     }
